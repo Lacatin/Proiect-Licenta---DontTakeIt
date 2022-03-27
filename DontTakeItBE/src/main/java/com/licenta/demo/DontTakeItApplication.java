0162265758bf0dbbdeb8d38f37443cd1b94df0dc
Lacatin <florin_catalin.cismaru@yahoo.com>
2022-03-27 19:37:09 +0300

    Initial push for BE

---
+++ DontTakeItBE/src/main/java/com/licenta/demo/DontTakeItApplication.java
@@ -0,0 +1,13 @@
+package com.licenta.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontTakeItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontTakeItApplication.class, args);
+	}
+
+}
