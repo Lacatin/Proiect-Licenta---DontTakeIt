517130480612e8f2470184bc864b03cb670e6807
Lacatin <florin_catalin.cismaru@yahoo.com>
2022-05-01 20:49:58 +0300

    Sidebar-ul functioneaza

--- DontTakeItFE/DontTakeItFE/src/app/app-routing.module.ts
+++ DontTakeItFE/DontTakeItFE/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardStudentiComponent } from './dashboard-studenti/dashboard-studenti.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path:'studenti',
+  component: DashboardStudentiComponent,
+  pathMatch: 'full'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
