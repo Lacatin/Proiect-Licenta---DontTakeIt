a7faf967710eaa8e9c86547d67c4d8efb8b9dcd8
Lacatin <florin_catalin.cismaru@yahoo.com>
2022-06-07 15:25:28 +0300

    Crearea paginii de comparare a lucrarilor

--- DontTakeItFE/src/app/pages/studenti-comparare/studenti-comparare.component.ts
+++ DontTakeItFE/src/app/pages/studenti-comparare/studenti-comparare.component.ts
@@ -13,12 +13,18 @@ import { RestService } from '~shared/services/rest-service';
 export class StudentiComparareComponent implements OnInit {
 
   studenti: Student[] = [];
+
   student1: Student;
+  student2: Student;
   lucrariStudent1: string[] = [];
+  lucrariStudent2: string[] = [];
+
   formGroup: FormGroup;
 
   studentiNrMatricolAOptions: Observable<string[]> | undefined;
   studentiLucrari1Options: Observable<string[]> | undefined;
+  studentiNrMatricolBOptions: Observable<string[]> | undefined;
+  studentiLucrari2Options: Observable<string[]> | undefined;
   studentiNrMatricol: string[] = [];
 
   constructor(
@@ -43,7 +49,7 @@ export class StudentiComparareComponent implements OnInit {
     this.formGroup = this.formBuilder.group({
       nrMatricolA:     [''],
       lucrare1:        [''],
-      nrMatricol2:     [''],
+      nrMatricolB:     [''],
       lucrare2:        [''],
     });
 
@@ -61,6 +67,16 @@ export class StudentiComparareComponent implements OnInit {
     this.studentiLucrari1Options = this.formGroup.get('lucrare1')?.valueChanges.pipe(
       startWith(''),
       map(value => this.studentiLucrare1Filter(value))
+    );
+
+    this.studentiNrMatricolBOptions = this.formGroup.get('nrMatricolB')?.valueChanges.pipe(
+      startWith(''),
+      map(value => this.studentiNrMatricolBFilter(value))
+    );
+
+    this.studentiLucrari2Options = this.formGroup.get('lucrare2')?.valueChanges.pipe(
+      startWith(''),
+      map(value => this.studentiLucrare2Filter(value))
     );
 
   }
@@ -93,6 +109,34 @@ export class StudentiComparareComponent implements OnInit {
     }
   }
 
+  studentiNrMatricolBFilter(value: string): string[] {
+    if (value == null) {
+      let studentiNrMatricolTemp = this.studentiNrMatricol;
+      return studentiNrMatricolTemp;
+    } else {
+      if (value === "") {
+        return this.studentiNrMatricol;
+      }
+      let studentiArray = [...this.studentiNrMatricol];
+      let studentiNrMatricolOptionsTemp = studentiArray.filter(option => option.toLowerCase().includes(value.toLowerCase()));
+      return studentiNrMatricolOptionsTemp;
+    }
+  }
+
+  studentiLucrare2Filter(value: string): string[] {
+    if (value == null) {
+      let studentiLucrare1Temp = this.lucrariStudent2;
+      return studentiLucrare1Temp;
+    } else {
+      if (value === "") {
+        return this.lucrariStudent2;
+      }
+      let studentiArray = [...this.lucrariStudent2];
+      let studentiLucrare1OptionsTemp = studentiArray.filter(option => option.toLowerCase().includes(value.toLowerCase()));
+      return studentiLucrare1OptionsTemp;
+    }
+  }
+
   public checkIfNrMatricolAIsEmpty(): boolean {
     let dataForm = this.formGroup.value;
     return !(dataForm.nrMatricolA === "" || dataForm.nrMatricolA === null);
@@ -103,27 +147,50 @@ export class StudentiComparareComponent implements OnInit {
     return !(dataForm.lucrare1 === "" || dataForm.lucrare1 === null);
   }
 
+  public checkIfNrMatricolBIsEmpty(): boolean {
+    let dataForm = this.formGroup.value;
+    return !(dataForm.nrMatricolB === "" || dataForm.nrMatricolB === null);
+  }
+
+  public checkIfLucrari2IsEmpty(): boolean {
+    let dataForm = this.formGroup.value;
+    return !(dataForm.lucrare2 === "" || dataForm.lucrare2 === null);
+  }
+
   resetSpecificFilter(filterName:string): void {
     this.formGroup.controls[filterName].reset();
   }
 
   getStudentByNrMatricol(numarMatricol: string) {
-    console.log(numarMatricol);
-    
     return this.studenti.filter(x => x.numarMatricol === numarMatricol);
   }
 
   submitNrMatricolA() {   
+
     if(this.getStudentByNrMatricol(this.formGroup.value['nrMatricolA'])[0] !== null && this.getStudentByNrMatricol(this.formGroup.value['nrMatricolA'])[0] !== undefined) {
       this.student1 = this.getStudentByNrMatricol(this.formGroup.value['nrMatricolA'])[0];
       for(let lucrare of this.student1.lucrari) {
         this.lucrariStudent1.push(lucrare.nume);
       }
-      console.log("Lucrari: ", this.lucrariStudent1);
       
       this.initAutocompleteObs();
     } else {
       this.lucrariStudent1 = [];
+    }
+
+  }
+
+  submitNrMatricolB() {   
+    
+    if(this.getStudentByNrMatricol(this.formGroup.value['nrMatricolB'])[0] !== null && this.getStudentByNrMatricol(this.formGroup.value['nrMatricolB'])[0] !== undefined) {
+      this.student2 = this.getStudentByNrMatricol(this.formGroup.value['nrMatricolB'])[0];
+      for(let lucrare of this.student2.lucrari) {
+        this.lucrariStudent2.push(lucrare.nume);
+      }
+      
+      this.initAutocompleteObs();
+    } else {
+      this.lucrariStudent2 = [];
     }
 
   }
